2726de92097028b775f50adb9cad74e010a83a59
Федор Филиппов <fefilippov@edu.hse.ru>
2022-12-15 23:07:33 +0300

    fix

--- Fuctivity/Extensions/ColorFromHexExtension.swift
+++ Fuctivity/Extensions/ColorFromHexExtension.swift
@@ -2,13 +2,21 @@
 //  ColorFromHexExtension.swift
 //  Fuctivity
 //
-//  Created by Федор Филиппов on 10.12.2022.
+//  Created by Sosin Vladislav on 14.12.2022.
 //
 
-import Foundation
 import UIKit
 
 extension UIColor {
+    convenience init(hex: Int) {
+        let components = (
+            R: CGFloat((hex >> 16) & 0xff) / 255,
+            G: CGFloat((hex >> 08) & 0xff) / 255,
+            B: CGFloat((hex >> 00) & 0xff) / 255
+        )
+        self.init(red: components.R, green: components.G, blue: components.B, alpha: 1)
+    }
+    
     static func UIColorFromRGB(rgbValue: UInt) -> UIColor {
         return UIColor(
             red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
